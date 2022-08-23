33014be2cc3740067b9156d8da709475d45c0e00
xucxichchien <111264991+xucxichchien@users.noreply.github.com>
2022-08-23 15:21:25 +0700

    Add files via upload

---
+++ lara0905a.sql
@@ -0,0 +1,123 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th8 23, 2022 lúc 10:19 AM
+-- Phiên bản máy phục vụ: 10.4.24-MariaDB
+-- Phiên bản PHP: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `lara0905a`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `admins`
+--
+-- Error reading structure for table lara0905a.admins: #1932 - Table &#039;lara0905a.admins&#039; doesn&#039;t exist in engine
+-- Error reading data for table lara0905a.admins: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `lara0905a`.`admins`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `brands`
+--
+-- Error reading structure for table lara0905a.brands: #1932 - Table &#039;lara0905a.brands&#039; doesn&#039;t exist in engine
+-- Error reading data for table lara0905a.brands: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `lara0905a`.`brands`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `categories`
+--
+-- Error reading structure for table lara0905a.categories: #1932 - Table &#039;lara0905a.categories&#039; doesn&#039;t exist in engine
+-- Error reading data for table lara0905a.categories: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `lara0905a`.`categories`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `failed_jobs`
+--
+-- Error reading structure for table lara0905a.failed_jobs: #1932 - Table &#039;lara0905a.failed_jobs&#039; doesn&#039;t exist in engine
+-- Error reading data for table lara0905a.failed_jobs: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `lara0905a`.`failed_jobs`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `managers`
+--
+-- Error reading structure for table lara0905a.managers: #1932 - Table &#039;lara0905a.managers&#039; doesn&#039;t exist in engine
+-- Error reading data for table lara0905a.managers: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `lara0905a`.`managers`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `migrations`
+--
+-- Error reading structure for table lara0905a.migrations: #1932 - Table &#039;lara0905a.migrations&#039; doesn&#039;t exist in engine
+-- Error reading data for table lara0905a.migrations: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `lara0905a`.`migrations`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `orderdetails`
+--
+-- Error reading structure for table lara0905a.orderdetails: #1932 - Table &#039;lara0905a.orderdetails&#039; doesn&#039;t exist in engine
+-- Error reading data for table lara0905a.orderdetails: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `lara0905a`.`orderdetails`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `orders`
+--
+-- Error reading structure for table lara0905a.orders: #1932 - Table &#039;lara0905a.orders&#039; doesn&#039;t exist in engine
+-- Error reading data for table lara0905a.orders: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `lara0905a`.`orders`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `parfums`
+--
+-- Error reading structure for table lara0905a.parfums: #1932 - Table &#039;lara0905a.parfums&#039; doesn&#039;t exist in engine
+-- Error reading data for table lara0905a.parfums: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `lara0905a`.`parfums`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `password_resets`
+--
+-- Error reading structure for table lara0905a.password_resets: #1932 - Table &#039;lara0905a.password_resets&#039; doesn&#039;t exist in engine
+-- Error reading data for table lara0905a.password_resets: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `lara0905a`.`password_resets`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `personal_access_tokens`
+--
+-- Error reading structure for table lara0905a.personal_access_tokens: #1932 - Table &#039;lara0905a.personal_access_tokens&#039; doesn&#039;t exist in engine
+-- Error reading data for table lara0905a.personal_access_tokens: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `lara0905a`.`personal_access_tokens`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `users`
+--
+-- Error reading structure for table lara0905a.users: #1932 - Table &#039;lara0905a.users&#039; doesn&#039;t exist in engine
+-- Error reading data for table lara0905a.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `lara0905a`.`users`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
